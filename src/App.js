20defbe0c125d0da7666bfa4921de145598f8721
soubj <sobujahmed124@gmail.com>
2023-04-06 16:31:09 +0600

    one way of getting data from api in react

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+function App() {
+  return <div className="App">{/* <LoadCountries></LoadCountries> */}</div>;
+}
+
+// function LoadCountries() {
+//   const [countries, setCountries] = useState([]);
+
+//   useEffect(() => {
+//     fetch("https://restcountries.com/v3.1/all")
+//       .then((res) => res.json())
+//       .then((data) => setCountries(data));
+//   }, []);
+
+//   return (
+//     <div>
+//       {countries.map((country) => (
+//         <Country name={country.name.common}></Country>
+//       ))}
+//     </div>
+//   );
+// }
+
+// function Country(props) {
+//   return (
+//     <div>
+//       <h2>country name: {props.name}</h2>
+//     </div>
+//   );
+// }
+
+export default App;
