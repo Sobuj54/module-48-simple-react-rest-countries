f338c0947b59e9abcbae9e82c23fe0545de61318
soubj <sobujahmed124@gmail.com>
2023-04-06 20:04:59 +0600

    fixed minor bug

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -4,7 +4,7 @@ import "./Country.css";
 const Country = (props) => {
   return (
     <div className="country">
-      <h2>Country Name: {props.country.name}</h2>
+      <h2>Country Name: {props.country.name.common}</h2>
 
       <h3>Region: {props.country.region}</h3>
       <h4>Population: {props.country.population}</h4>
